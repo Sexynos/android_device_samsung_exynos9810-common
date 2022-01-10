812d9c3f8a58f7665dfabfa914fc054902bfc91f
Dylan Neve <dylanneve1@gmail.com>
2022-01-10 16:07:04 +0000

    universal9810: Build Memtrack service from source
    Change-Id: I3f55d828bce087a438d1d4ac7220879a10f37677

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -123,6 +123,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
